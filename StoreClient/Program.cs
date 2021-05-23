aafc9157ac75c5e50530f5f01afc66f9cb2ca6d3
Alexey Levashev <alexey.levashev@tl-istu.com>
2021-05-23 12:59:00 +0200

    Initial commit

---
+++ StoreClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
